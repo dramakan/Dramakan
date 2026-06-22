7d64b94c2588140365f9c97eabb8e6aa377531c1
Dramakan <zabaazcreations@gmail.com>
2026-06-22 12:55:23 +0530

    fwhjkbwehjhbfk

--- sw.js
+++ sw.js
@@ -8,3 +8,9 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 11185210
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
