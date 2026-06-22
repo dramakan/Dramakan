25f59bb5019bdd1f44265959f175af866074f4bd
Dramakan <zabaazcreations@gmail.com>
2026-06-22 12:53:08 +0530

    monetag

--- sw.js
+++ sw.js
@@ -2,3 +2,9 @@
 self.addEventListener('fetch', (event) => {
   // This can be empty, but it must exist
 });
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11185192
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
